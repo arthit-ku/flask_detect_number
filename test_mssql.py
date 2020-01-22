9b44a36fa653bfe9782abf175a6b61f628324975
arthit75420 <arthit.n@ku.th>
2020-01-22 20:16:05 +0700

    .2.2

--- test_mssql.py
+++ test_mssql.py
@@ -8,11 +8,13 @@ dbname = "NSP"
 conn = pymssql.connect(database='NSP', user='sa',
                        password='prim', host='192.168.10.2', port=1433)
 cursor = conn.cursor()
-sql = "SELECT PartNo, PartCode FROM lot L INNER JOIN product P ON L.ProductID = P.ProductID WHERE LotNo = '{}' ;".format(
+sql = "SELECT PartNo, PartCode, StartQty FROM lot L INNER JOIN product P ON L.ProductID = P.ProductID WHERE LotNo = '{}' ;".format(
     LotNO)
 cursor.execute(sql)
 row = cursor.fetchone()
 Return = {}
 Return["PartNo"] = row[0]
 Return["PartCode"] = row[1]
+Return["SampleQty"] = row[2]
+Return["LotNO"] = LotNO
 print(Return)
