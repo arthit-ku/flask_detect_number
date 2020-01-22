b42b571cc4f50d1c705e023416df378d493b6b5a
SAN <42034332+arthit75420@users.noreply.github.com>
2020-01-22 12:06:40 +0700

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -27,13 +27,15 @@ def getmis():
     conn = pymssql.connect(database='NSP', user='sa',
                            password='prim', host='192.168.10.2', port=1433)
     cursor = conn.cursor()
-    sql = "SELECT PartNo, PartCode FROM lot L INNER JOIN product P ON L.ProductID = P.ProductID WHERE LotNo = '{}' ;".format(
+    sql = "SELECT PartNo, PartCode, StartQty FROM lot L INNER JOIN product P ON L.ProductID = P.ProductID WHERE LotNo = '{}' ;".format(
         LotNO)
     cursor.execute(sql)
     row = cursor.fetchone()
     Return = {}
-    Return["PartNo"] = row[0]
+    Return["PartNO"] = row[0]
     Return["PartCode"] = row[1]
+    Return["StartQty"] = row[2]
+    Return["LotNO"] = LotNO
     return jsonify(Return)
 
 
